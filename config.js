0ce26fbf2b03d90e92b86a4eb8f753efabe3f33b
picafern-commits <pica.fern@gmail.com>
2026-06-22 17:28:38 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.MUNDIAL_CONFIG = {
-  appVersion: "139.0",
+  appVersion: "140.0",
   adminPin: "1234",
   adminEmails: ["pica.fern@gmail.com"],
   firebase: {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mundial-pontos-2026-v139";
+const CACHE_NAME = "mundial-pontos-2026-v140";
 const APP_SHELL = [
   "./",
   "./index.html",
