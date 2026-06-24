cbfe4ed0c062e4a48107f5f08c0b78575263b8cf
picafern-commits <pica.fern@gmail.com>
2026-06-24 15:04:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mundial-pontos-2026-v215-organizacao-unica-select-fix";
+const CACHE_NAME = "mundial-pontos-2026-v216-tabs-organizacao-select";
 const APP_SHELL = [
   "./",
   "./index.html",
