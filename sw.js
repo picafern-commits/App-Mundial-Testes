61bd9d8b7dbb3909868ff676d3238c8dda27f0db
picafern-commits <pica.fern@gmail.com>
2026-06-25 00:16:36 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mundial-pontos-2026-v233-knockout-visual-fit";
+const CACHE_NAME = "mundial-pontos-2026-v234-knockout-locked-grid";
 const APP_SHELL = [
   "./",
   "./index.html",
