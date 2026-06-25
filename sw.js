3eff9b833ee768d1b4dc2f46ad64b5062036da91
picafern-commits <pica.fern@gmail.com>
2026-06-25 17:20:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mundial-pontos-2026-v245-fase-final-obrigatorio";
+const CACHE_NAME = "mundial-pontos-2026-v246-fase-final-fullscreen";
 const APP_SHELL = [
   "./",
   "./index.html",
